56682ef8fec8fe3367213689a890e4fe78ea746d
Lakshya Mudgal <lakshya2005mudgal@gmail.com>
2024-11-06 00:11:56 +0530

    exploring put , delete and post using postman

--- src/app.js
+++ src/app.js
@@ -3,9 +3,26 @@ const express=require("express");
 const app=express();
 
 
-app.use("/test",(req,res)=>{
-    res.send("hello from the server")
+app.get("/user",(req,res)=>{
+    res.send({firstname:"lakshya",
+                secondname:"mudgal",
+                age:"can't tell",
+    });
 });
+
+app.post("/user",(req,res)=>{
+    
+    res.send("data saved to database successfully")
+});
+
+app.delete("/user",(req,res)=>{
+       res.send("data deleted")
+});
+
+app.use("/test",(req,res)=>{
+    res.send("hello from test")
+});
+
 
 app.listen(3000,()=>{
     console.log("server is listening ")
