425d4806d1d5a0a6aad5714e0875fb3a4f20652a
Lakshya Mudgal <lakshya2005mudgal@gmail.com>
2024-12-16 08:18:25 +0530

    made some changes in code

--- src/app.js
+++ src/app.js
@@ -2,19 +2,40 @@ const express = require('express');
 
 const app = express();
 
-app.use("/user",
-    (req,res,next)=>{
-    //route handler 1 
-    console.log("handling the route user ");
-    // res.send("Response"); 
-    //if next was not used it would not work and an infinite loop would have started .
+// app.use("/user",
+//     (req,res,next)=>{
+//     //route handler 1 
+//     console.log("handling the route user ");
+//     // res.send("Response"); 
+//     //if next was not used it would not work and an infinite loop would have started .
+//     next();
+// },
+//     (req,res)=>{
+//         console.log("handling the route user 2 ");
+//         res.send("2nd response");
+//     }
+// );
+
+
+app.use("/",(req,res,next)=>{
+    res.send("response from use ");
     next();
-},
-    (req,res)=>{
-        console.log("handling the route user 2 ");
-        res.send("2nd response");
+})
+
+
+app.get("/user", 
+    (req,res,next)=>{
+        console.log("handlng /user route ");
+        next();
+    },
+    (req,res,next)=>{
+        next();
+    },
+    (req,res,next)=>{
+        // res.send("2nd route handler");
     }
 );
+
 
 app.listen(3333,()=>{
     console.log("app listening at 3333");
