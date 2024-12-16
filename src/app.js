a7e9769b3ebdaaaa3bb8b7d1389f7c818d450e00
Lakshya Mudgal <lakshya2005mudgal@gmail.com>
2024-12-16 23:28:42 +0530

    without using middleware

--- src/app.js
+++ src/app.js
@@ -2,40 +2,18 @@ const express = require('express');
 
 const app = express();
 
-// app.use("/user",
-//     (req,res,next)=>{
-//     //route handler 1 
-//     console.log("handling the route user ");
-//     // res.send("Response"); 
-//     //if next was not used it would not work and an infinite loop would have started .
-//     next();
-// },
-//     (req,res)=>{
-//         console.log("handling the route user 2 ");
-//         res.send("2nd response");
-//     }
-// );
-
-
-// app.use("/",(req,res,next)=>{
-//     res.send("response from use ");
-//     next();
-// })
-app.use("/hello",(req,res,next)=>{
-    res.send("response form hello");
-    next()
+app.get("/admin/getData",(req,res,next)=>{
+const token="xyz";
+const isAdminAuthorized = token ==="xyz";
+if(isAdminAuthorized){
+    res.send("authorized user");
+    console.log("working");
+}
+else{
+    res.status(401);
+    console.log("unauthorized user");
+}
 });
-
-app.get("/user",(req,res)=>{
-    res.send({firstName:"lakshya", lastName:"mudgal"});
-});
-
-
-app.post("/user",(req,res,next)=>{
-    res.send("hheheehehehehe");
-    next();
-});
-
 
 app.listen(3333,()=>{
 console.log("listening to 3333");
