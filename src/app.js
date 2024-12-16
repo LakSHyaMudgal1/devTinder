73aa754ae38fce30e99e2194f0086c0c11a3f336
Lakshya Mudgal <lakshya2005mudgal@gmail.com>
2024-12-17 00:37:42 +0530

    middlewares

--- src/app.js
+++ src/app.js
@@ -2,18 +2,14 @@ const express = require('express');
 
 const app = express();
 
+const {adminAuth}=require('./Middlewares/auth')
+
+app.use("/admin", adminAuth);
+
 app.get("/admin/getData",(req,res,next)=>{
-const token="xyz";
-const isAdminAuthorized = token ==="xyz";
-if(isAdminAuthorized){
-    res.send("authorized user");
-    console.log("working");
-}
-else{
-    res.status(401);
-    console.log("unauthorized user");
-}
+    res.send("all data sent ");
 });
+
 
 app.listen(3333,()=>{
 console.log("listening to 3333");
