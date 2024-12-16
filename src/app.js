f59d78c9c09a28ab1ca8357f99dc74a249e264ab
Lakshya Mudgal <lakshya2005mudgal@gmail.com>
2024-12-16 08:13:27 +0530

    1

--- src/app.js
+++ src/app.js
@@ -1,29 +1,21 @@
-const express=require("express");
+const express = require('express');
 
-const app=express();
+const app = express();
 
+app.use("/user",
+    (req,res,next)=>{
+    //route handler 1 
+    console.log("handling the route user ");
+    // res.send("Response"); 
+    //if next was not used it would not work and an infinite loop would have started .
+    next();
+},
+    (req,res)=>{
+        console.log("handling the route user 2 ");
+        res.send("2nd response");
+    }
+);
 
-app.get("/user",(req,res)=>{
-    res.send({firstname:"lakshya",
-                secondname:"mudgal",
-                age:"can't tell",
-    });
-});
-
-app.post("/user",(req,res)=>{
-    
-    res.send("data saved to database successfully")
-});
-
-app.delete("/user",(req,res)=>{
-       res.send("data deleted")
-});
-
-app.use("/test",(req,res)=>{
-    res.send("hello from test")
-});
-
-
-app.listen(3000,()=>{
-    console.log("server is listening ")
-});
+app.listen(3333,()=>{
+    console.log("app listening at 3333");
+})
